65ed69b1091713240d7b455d9513bdb4c97386fa
Mohammed Al-hameen <mohammedalhameen@gmail.com>
2023-07-26 10:36:47 +0100

    Section1-Completed

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className='nav-container'>
+      <a href='/#'>
+        Home
+      </a>
+
+      <a href='/#'>
+        About
+      </a>
+
+      <a href='/#'>
+        Projects
+      </a>
+
+      <a href='/#'>
+        Contacts
+      </a>
+    </div>
+  )
+}
+
+export default NavBar
--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Section1.css'
+import NavBar from '../NavBar/NavBar'
 const Section1 = () => {
   return (
     <div className='section1-container'>
+      <NavBar/>
 
+      <div className='section1-content'>
+        <h2>
+          Make it Happen
+        </h2>
+
+        <p>
+          Driving technology to the future with you along!
+        </p>
+      </div>
+
+      <div className='section1-button'>
+        <button>
+          <a href='https://sst.pau.edu.ng/'>
+            Get Started
+          </a>
+        </button>
+      </div>
     </div>
   )
 }
